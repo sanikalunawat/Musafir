cd4d8b6e42a0818712ff7006bae05cad84abcb02
Arya Madhani <madhaniarya1754@gmail.com>
2024-10-23 22:33:07 +0530

    fixed

--- ai-trip-planner-main/src/view-trip/components/ItineraryCard.jsx
+++ ai-trip-planner-main/src/view-trip/components/ItineraryCard.jsx
@@ -8,18 +8,26 @@ const ItineraryCard = ({ plan }) => {
   return (
     <Link to={`https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}&zoom=14`} target="_blank">
       <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden transition-transform transform hover:scale-105 duration-300 itinerary-card">
-        <img
+        {/* <img
           className="object-cover w-full h-48"
           src={plan?.placeImageURL || "../../src/assets/fallback.jpg"}
           alt={plan?.placeName}
-        />
+        /> */}
         <div className="p-4">
           <h3 className="text-xl font-bold">{plan?.placeName}</h3>
           <p className="text-gray-600">{plan?.placeDetails}</p>
           <div className="flex items-center mt-2">
-            <span className="text-md">{plan?.rating} <CiStar /></span>
-            <span className="ml-2">{plan?.ticketPricing}</span>
+          <span className="flex items-center">
+              {plan?.rating}
+              <CiStar />
+            </span>
+          <span className="ml-2">{plan?.ticketPricing}</span>
           </div>
+          {/* <span className="flex items-center">
+              {plan?.rating}
+              <CiStar />
+            </span>
+          <span className="ml-2">{plan?.ticketPricing}</span> */}
         </div>
       </div>
     </Link>
