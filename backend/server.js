f8c455ecfea65b8bba8cca9d4577f7c2c5c4d44f
Sanika Lunawat <140091874+sanikalunawat@users.noreply.github.com>
2025-06-01 00:47:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 console.log("Hello World");
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://travel_user:yourSecurePassword123@travelplannerdb.ad9pf.mongodb.net/?retryWrites=true&w=majority&appName=TravelPlannerDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('your_mongodb_url', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log('Error connecting to MongoDB:', error));
 
